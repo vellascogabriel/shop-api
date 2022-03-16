c5b3eda52c7f5e5345151e84f09babf411082987
vellasco <gabrielvellasco@id.uff.br>
2022-03-16 18:54:56 -0300

    product model

---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60, ErrorMessage = "O Título deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "O Título deve conter entre 3 e 60 caracteres")]
+        public string Title { get; set; }
+
+        [MaxLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, 999999, ErrorMessage = "Preço inválido")]
+        public int CategoryId   { get; set; }
+
+        public Category Category { get; set; }  
+
+    }
+}
