434238c90cffb19f19a14490932982b5cad17391
vellasco <gabrielvellasco@id.uff.br>
2022-03-16 20:22:00 -0300

    controllers and DbContext

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Shop.Models;
+using System.Threading.Tasks;
 
 namespace Shop.Controllers
 {
@@ -7,27 +10,46 @@ namespace Shop.Controllers
     public class CategoryController : ControllerBase
     {
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult<List<Category>>> Get()
         {
-            return "GET";
+            return new List<Category>();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById([FromQuery] int id)
+        {
+            return new Category();
         }
 
         [HttpPost]
-        public string Post()
+        public  async Task<ActionResult<List<Category>>> Post(int id, [FromBody] Category model)
         {
-            return "POST";
+            if (id != model.Id)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            //Verifica se os dados são validos
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(model);
         }
 
         [HttpPut]
-        public string Put()
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
         {
-            return "Put";
+            if (model.Id == id)
+            {
+                return Ok(model);
+            }
+            return NotFound();
         }
 
         [HttpDelete]
-        public string Delete()
+        public ActionResult Delete()
         {
-            return "DELETE";
+            return Ok();
         }
     }
 }
