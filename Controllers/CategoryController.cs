53c47d47c1e61946d729d2a33688c3c094800345
vellasco <gabrielvellasco@id.uff.br>
2022-03-16 19:08:38 -0300

    first route

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    [ApiController]
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public string MeuMetodo()
+        {
+            return "Olá Mundo!";
+        }
+    }
+}
